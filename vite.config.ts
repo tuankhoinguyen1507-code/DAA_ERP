1176e29b878013f9e7ed33eb28e1bf1a263db451
tuankhoinguyen1507-code <tuankhoinguyen15.07@gmail.com>
2026-05-03 16:28:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base:/DAA_ERP/',
+    base:'/DAA_ERP/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
